31b6d48f0bf00f76da70dd53979af9f42ee79c4c
Robert Kawecki <robert+public@rkaw.pl>
2024-08-05 22:30:50 +0200

    Encapsulate all sensitive material for ease of disposal

--- src/Vault.ts
+++ src/Vault.ts
@@ -1,5 +1,5 @@
 import { createSecretKey, KeyObject, randomUUID } from 'node:crypto';
-import { AES128OCB, AES256KeyWrap, DefaultEncDec, PlaintextForMetadataOnly, ScryptKDFv1 } from './cryptography';
+import { AES128OCB, AES256KeyWrap, CryptoProvider, Decryptor, DefaultEncDec, Encryptor, PlaintextForMetadataOnly, ScryptKDFv1 } from './cryptography';
 import { BlobStorage } from './storage';
 import { Entry } from './Entry';
 import { VaultAccess } from './types/VaultAccess';
@@ -7,14 +7,34 @@ import { EncryptionKey } from './types/EncryptionKey';
 import { EntryRepository } from './EntryRepository';
 import { CryptographyIncompatibleError, RootEntryMalformedError, RootEntryNotFoundError, VaultEntryNotFoundError, VaultNotUnlockedError } from './errors';
 
+const kEntryRepository = Symbol('kEntryRepository');
+const kInnerEncryptor = Symbol('kInnerEncryptor');
+const kInnerDecryptor = Symbol('kInnerDecryptor');
+const kEntries = Symbol('kEntries');
+
+class VaultSensitivePart {
+    public [kEntryRepository]: EntryRepository;
+    public [kInnerEncryptor]: Encryptor;
+    public [kInnerDecryptor]: Decryptor;
+    public [kEntries] = new Map<string, Entry>();
+    constructor(
+        outerProvider: CryptoProvider,
+        outerKey: KeyObject,
+        innerProvider: CryptoProvider,
+        innerKey: KeyObject,
+        storage: BlobStorage,
+    ) {
+        const outerCrypto = new DefaultEncDec(outerProvider, outerKey);
+        this[kEntryRepository] = new EntryRepository(outerCrypto, outerCrypto, storage);
+        const innerCrypto = new DefaultEncDec(innerProvider, innerKey);
+        this[kInnerEncryptor] = innerCrypto;
+        this[kInnerDecryptor] = innerCrypto;
+    }
+}
+
 export class Vault {
     protected readonly ROOT_ENTRY_ID = 'root';
-
-    protected unlocked = false;
-    protected outerKey: KeyObject | null = null;
-    protected innerKey: KeyObject | null = null;
-    // TODO: Extract all sensitive objects to VaultSensitivePart.
-    protected crypto = {
+    protected providers = {
         key: new AES256KeyWrap(),
         outer: new AES128OCB(),
         // TODO: ChaCha20-Poly1305 for inner crypto
@@ -22,8 +42,7 @@ export class Vault {
         kdf: new ScryptKDFv1(),
     };
     protected metaRepository: EntryRepository;
-    protected entryRepository: EntryRepository | null = null;
-    protected entries = new Map<string, Entry>();
+    protected sensitiveData: VaultSensitivePart | null = null;
     protected loadingErrors = new Map<string, Error>();
 
     constructor(
@@ -41,10 +60,10 @@ export class Vault {
     }
 
     async initializeNew(password: string) {
-        const { key: kek, salt } = this.crypto.kdf.deriveNewKey(password, this.crypto.key.keyBytes());
-        const keyEncryptor = new DefaultEncDec(this.crypto.key, kek);
-        const outerKey = this.crypto.outer.generateKey();
-        const innerKey = this.crypto.inner.generateKey();
+        const { key: kek, salt } = this.providers.kdf.deriveNewKey(password, this.providers.key.keyBytes());
+        const keyEncryptor = new DefaultEncDec(this.providers.key, kek);
+        const outerKey = this.providers.outer.generateKey();
+        const innerKey = this.providers.inner.generateKey();
         const vaultId = this.generateVaultId();
         const rootEntry = new Entry(
             this.ROOT_ENTRY_ID,
@@ -52,7 +71,7 @@ export class Vault {
             [],
             new VaultAccess({
                 vaultId: vaultId,
-                kdf: this.crypto.kdf.getName(),
+                kdf: this.providers.kdf.getName(),
                 saltBase64: salt.toString('base64'),
             }),
             [
@@ -76,12 +95,12 @@ export class Vault {
         if (!(access instanceof VaultAccess)) {
             throw new RootEntryMalformedError('Usage type must be VaultAccess');
         }
-        if (access.getDetails().kdf !== this.crypto.kdf.getName()) {
+        if (access.getDetails().kdf !== this.providers.kdf.getName()) {
             // NOTE: If we add more KDF versions, this shouldn't be an equality check.
             throw new CryptographyIncompatibleError(`Unsupported KDF ${access.getDetails().kdf}`);
         }
         const kdfSalt = Buffer.from(access.getSalt());
-        const kek = this.crypto.kdf.deriveKey(password, kdfSalt, this.crypto.key.keyBytes());
+        const kek = this.providers.kdf.deriveKey(password, kdfSalt, this.providers.key.keyBytes());
         const rootSecrets = rootEntry.getSecrets();
         const [ outerKeySecret, innerKeySecret ] = rootSecrets;
         if (!outerKeySecret || !innerKeySecret) {
@@ -93,38 +112,41 @@ export class Vault {
         if (!(innerKeySecret instanceof EncryptionKey)) {
             throw new RootEntryMalformedError('inner key is not an EncryptionKey');
         }
-        const keyDecryptor = new DefaultEncDec(this.crypto.key, kek);
-        this.outerKey = createSecretKey(
-            Buffer.from(outerKeySecret.reveal(keyDecryptor).base64, 'base64')
-        );
-        this.innerKey = createSecretKey(
-            Buffer.from(innerKeySecret.reveal(keyDecryptor).base64, 'base64')
-        );
-        const outer = new DefaultEncDec(this.crypto.outer, this.outerKey);
-        this.entryRepository = new EntryRepository(
-            outer,
-            outer,
+        const keyDecryptor = new DefaultEncDec(this.providers.key, kek);
+
+        this.sensitiveData = new VaultSensitivePart(
+            this.providers.outer,
+            createSecretKey(
+                Buffer.from(outerKeySecret.reveal(keyDecryptor).base64, 'base64')
+            ),
+            this.providers.inner,
+            createSecretKey(
+                Buffer.from(innerKeySecret.reveal(keyDecryptor).base64, 'base64')
+            ),
             this.entryStore,
-        )
-        this.unlocked = true;
+        );
+    }
+
+    isUnlocked() {
+        return Boolean(this.sensitiveData);
     }
 
     async loadEntry(id: string) {
-        if (!this.entryRepository) {
+        if (!this.sensitiveData) {
             throw new VaultNotUnlockedError();
         }
-        const entry = await this.entryRepository.load(id);
+        const entry = await this.sensitiveData[kEntryRepository].load(id);
         if (!entry) {
             throw new VaultEntryNotFoundError(id);
         }
-        this.entries.set(entry.getId(), entry);
+        this.sensitiveData[kEntries].set(entry.getId(), entry);
     }
 
     async loadEntries() {
-        if (!this.entryRepository) {
+        if (!this.sensitiveData) {
             throw new VaultNotUnlockedError();
         }
-        for (const id of await this.entryRepository.listKeys()) {
+        for (const id of await this.sensitiveData[kEntryRepository].listKeys()) {
             try {
                 await this.loadEntry(id);
             } catch (error) {
@@ -134,26 +156,22 @@ export class Vault {
     }
 
     getEntry(id: string): Entry | null {
-        if (!this.unlocked) {
+        if (!this.sensitiveData) {
             throw new VaultNotUnlockedError();
         }
-        return this.entries.get(id) ?? null;
+        return this.sensitiveData[kEntries].get(id) ?? null;
     }
 
     async saveEntry(entry: Entry): Promise<string> {
-        if (!this.entryRepository) {
+        if (!this.sensitiveData) {
             throw new VaultNotUnlockedError();
         }
-        await this.entryRepository.save(entry);
-        this.entries.set(entry.getId(), entry);
+        await this.sensitiveData[kEntryRepository].save(entry);
+        this.sensitiveData[kEntries].set(entry.getId(), entry);
         return entry.getId();
     }
 
     async lock() {
-        this.entries.clear();
-        this.entryRepository = null;
-        this.outerKey = null;
-        this.innerKey = null;
-        this.unlocked = false;
+        this.sensitiveData = null;
     }
 }
